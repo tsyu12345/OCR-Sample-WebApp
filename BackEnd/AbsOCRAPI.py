8a7d4d45b96bedaefa77c0720f7da52c655d6e89
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-10-27 15:14:30 +0900

    【バックエンド】環境をpipenv→poetryに移行。かくOCRライブラリの抽象クラスを追加。

---
+++ BackEnd/AbsOCRAPI.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta
+from abc import abstractmethod
+from pydantic import BaseModel
+
+from PIL import Image
+
+
+
+class AbsOCRAPI(metaclass = ABCMeta):
+    """_summary_
+    OCR APIの抽象クラス
+    """
+
+    def __init__(self,):
+        pass
+
+    @abstractmethod
+    def read_img(self, img: Image) -> None:
+        """
+        画像からOCRを行い、結果を返す
+        Args:
+            img (Image): 入力画像
+        """
+        pass
